61b93c221671771d4506dd51d53d632606fb87c1
Kavan Dagli <115869568+K-D-26@users.noreply.github.com>
2023-10-24 20:17:54 +0530

    day 2

---
+++ day 2/repeatingAndMissingNumber.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static int[] missingAndRepeating(ArrayList<Integer> list, int n) {
+        long sn = (n * (n+1)) / 2;                  // sum of n numbers
+        long s2n = (n * (n+1) * (2*n+1)) / 6;       // sum of squares of n numbers
+        long s = 0, s2 = 0;
+        for(int i=0; i<n; i++) {
+            s += list.get(i);
+            s2 += list.get(i) * list.get(i);
+        }
+
+        long val1 = s - sn;         // will store repeating - missing
+        long val2 = s2 - s2n;       // will store repeating^2 - missing^2 = (repeating - missing) * (repeating + missing)
+        val2 = val2 / val1;         // will update to (repeating + missing)
+        long r = (val1 + val2) / 2; 
+        long m = r - val1;
+        return new int[]{(int)m, (int)r};
+
+
+        // int[] ans = new int[2];
+        // boolean[] arr = new boolean[n];
+        // for(int i=0; i<n; i++) {
+        //     if(arr[list.get(i)-1] == true) {
+        //         ans[1] = list.get(i);
+        //     }
+        //     arr[i] = true;
+        // }
+
+        // for(int i=0; i<n; i++) {
+        //     if(!arr[i] == false) {
+        //         ans[0] = i+1;
+        //         break;
+        //     }
+        // }
+
+        // return ans;
+    }
+}
---
+++ day 2/rotateMatrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        // transpose
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = i; j < matrix[0].length; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        
+        // reverse all rows
+        for(int i = 0; i < matrix.length; i++) {
+            reverse(matrix[i]);
+        }
+    }
+    
+    private static void reverse(int[] a) {
+        int l = 0, r = a.length - 1;
+        while(l < r) {
+            int t = a[l];
+            a[l] = a[r];
+            a[r] = t;
+            l++;
+            r--;
+        }
+    }
+}
