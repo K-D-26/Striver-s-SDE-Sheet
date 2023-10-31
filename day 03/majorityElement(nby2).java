c1dcffdae27af23608d6b6da3a9ffa6a2cfa987a
Kavan Dagli <115869568+K-D-26@users.noreply.github.com>
2023-10-31 13:15:09 +0530

    day 03

---
+++ day 03/majorityElement(nby2).java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        // Moore voting algo
+        int cnt = 0, ans = 0;
+
+        for (int i : nums) {
+            if (cnt == 0) {
+                ans = i;
+            }
+            if (i == ans) {
+                cnt++;
+            }
+            else {
+                cnt--;
+            }
+        }
+        
+        return ans;
+    }
+}
